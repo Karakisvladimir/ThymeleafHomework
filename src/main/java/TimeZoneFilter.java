8cd3aaa8df8cf71cf4727e81ce2b19eea0903e1e
Vladimir <karakisvladimir@gmail.com>
2023-11-22 00:20:58 +0200

    Initial commit

---
+++ src/main/java/TimeZoneFilter.java
@@ -0,0 +1,34 @@
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.ZoneId;
+import java.util.Set;
+
+@WebFilter("/time")
+public class TimeZoneFilter extends HttpFilter {
+  @Override
+    public void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        String timezoneParam = request.getParameter("timezone");
+
+        if (timezoneParam == null || isValidTimezone(timezoneParam)) {
+            chain.doFilter(request, response);
+        } else {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            response.getWriter().println("Invalid timezone");
+            response.getWriter().println("Please write zone in valid format");
+            response.getWriter().close();
+        }
+    }
+
+    private boolean isValidTimezone(String timezone) {
+        String zone = timezone.replace("UTC+", "Etc/GMT-").replace("UTC-", "Etc/GMT+");
+        Set<String> availableTimezones = ZoneId.getAvailableZoneIds();
+
+        return availableTimezones.contains(zone);
+    }
+}
